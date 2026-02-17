87f29dd6543b7e6b20f55beacbc535bf95f824a9
dee-diaz <diazthediablo@yahoo.com>
2026-02-17 17:12:41 -0300

    feat(Home): add CardGrid component and wrapper layout

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,17 @@
 import styles from './Home.module.css';
 import SortDropdown from '../../components/SortDropdown/SortDropdown';
 import FilterSidebar from '../../components/FilterSidebar/FilterSidebar';
+import CardGrid from '../../components/CardGrid/CardGrid';
 
 export default function HomePage() {
   return (
     <div className={`container ${styles.container}`}>
       <h1 className={styles.heading}>Vinyl catalog</h1>
       <SortDropdown />
-      <FilterSidebar />
+      <div className={styles.wrapper}>
+        <FilterSidebar />
+        <CardGrid />
+      </div>
     </div>
   );
 }
