64c18883db6f7ee89dfd43bd73378f91cee220c4
dee-diaz <diazthediablo@yahoo.com>
2026-02-18 16:56:13 -0300

    feat(CardGrid): add CardGrid component

---
+++ src/components/CardGrid/CardGrid.jsx
@@ -0,0 +1,18 @@
+import styles from './CardGrid.module.css';
+import Card from '../Card/Card';
+
+export default function CardGrid() {
+  return (
+    <div className={styles.cardGrid}>
+      <Card albumTitle="MM..Food" albumArtist="MF Doom" price="29" />
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
