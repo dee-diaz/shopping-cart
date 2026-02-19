34dabf5c0bbb514bac76a0350ee6e5102fd22787
dee-diaz <diazthediablo@yahoo.com>
2026-02-19 11:31:30 -0300

    feat(Button): add disabled prop support

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,12 @@
 import styles from './Button.module.css';
 
-export default function Button({ label, icon, size, onClick }) {
+export default function Button({ label, icon, size, isDisabled, onClick }) {
   const classes = [styles.btnPrimary, size === 'sm' && styles.sm]
     .filter(Boolean)
     .join(' ');
 
   return (
-    <button className={classes} onClick={onClick}>
+    <button className={classes} disabled={isDisabled} onClick={onClick}>
       {label}
       {icon && <span className={styles.icon}>{icon}</span>}
     </button>
