a5544d41efb695bb96f7a4ec78f87a5dc832ceb1
dee-diaz <diazthediablo@yahoo.com>
2026-02-27 13:34:11 -0300

    feat(ErrorMessage): add component with styles and tests

---
+++ frontend/src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+import styles from './ErrorMessage.module.css';
+
+export default function ErrorMessage({ message }) {
+  return (
+    <div role="alert" className={styles.error}>
+      <h2 className={styles.title}>Whoops, something went wrong</h2>
+      <p className={styles.text}>{`Error message: ${message}`}</p>
+    </div>
+  );
+}
