50075c1a9f0b552aca94bce68071d5359ca25f93
dee-diaz <diazthediablo@yahoo.com>
2026-02-27 16:11:13 -0300

    feat(CardGrid): connect to AlbumsContext and map real Discogs data

--- frontend/src/components/CardGrid/CardGrid.jsx
+++ frontend/src/components/CardGrid/CardGrid.jsx
@@ -1,7 +1,11 @@
 import styles from './CardGrid.module.css';
 import Card from '../Card/Card';
+import { useContext } from 'react';
+import { AlbumsContext } from '../../contexts/AlbumsContext';
 
-export default function CardGrid({ albums, variant }) {
+export default function CardGrid({ page, variant }) {
+  const { albums } = useContext(AlbumsContext);
+
   return (
     <div
       className={
@@ -9,20 +13,27 @@ export default function CardGrid({ albums, variant }) {
       }
       data-testid="card-grid"
     >
-      {albums.map((album) => {
-        return (
-          <Card
-            key={album.id}
-            albumTitle={album.title}
-            albumArtist={album.artist}
-            coverImgUrl={album.coverImgUrl}
-            price={album.price}
-            onAddToCart={() =>
-              console.log(`${album.title} has been added to Cart`)
-            }
-          />
-        );
-      })}
+      {page === 'home' &&
+        albums.map((album) => {
+          return (
+            <Card
+              key={album.id}
+              albumTitle={album.title}
+              albumArtist={album.artists[0].name}
+              coverImgUrl={album.images[0].uri}
+              price={generatePrice(album.lowest_price)}
+              onAddToCart={() =>
+                console.log(`${album.title} has been added to Cart`)
+              }
+            />
+          );
+        })}
     </div>
   );
+}
+
+function generatePrice(lowestPrice) {
+  const margin = 20;
+  if (lowestPrice) return `$${Math.round(lowestPrice) + margin}`;
+  return `$${margin + 11}`;
 }
