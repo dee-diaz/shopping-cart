f08506486aa1e2ccb85814346d20805dbf03f655
dee-diaz <diazthediablo@yahoo.com>
2026-03-02 12:37:37 -0300

    feat(Card): add AddToCartButton, toggle cart item, and link to product by id

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,32 +2,43 @@ import styles from './Card.module.css';
 import { Link } from 'react-router';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/CartContext';
+import AddToCartButton from '../Button/AddToCartButton';
+import { ADD_BTN_VARIANT } from '../../constants/constants';
 
 export default function Card({
   albumId,
+  genre,
   albumTitle,
   albumArtist,
   coverImgUrl,
   price,
 }) {
-  const { setCartItems } = useContext(CartContext);
+  const { cartItems, setCartItems } = useContext(CartContext);
   const altText = `${albumTitle} by ${albumArtist} album cover`;
 
-  function onAddToCart() {
+  function handleClick() {
+    if (cartItems.find((el) => el.id === albumId)) {
+      const newArr = cartItems.filter((el) => el.id !== albumId);
+      setCartItems(newArr);
+      return;
+    }
+
     const obj = {
       id: albumId,
+      genre,
       title: albumTitle,
       artist: albumArtist,
       coverImgUrl,
       price,
       quantity: 1,
     };
+
     setCartItems((prev) => [...prev, obj]);
   }
 
   return (
     <article className={styles.card}>
-      <Link to="/product">
+      <Link to={`/product/${albumId}`}>
         <img
           src={coverImgUrl || '/images/album-placeholder.webp'}
           alt={altText}
@@ -35,7 +46,7 @@ export default function Card({
         />
       </Link>
       <div className={styles.albumInfo}>
-        <Link to="/product">
+        <Link to={`/product/${albumId}`}>
           <div className={styles.albumDetails}>
             <h3>{albumTitle}</h3>
             <p>{albumArtist}</p>
@@ -49,49 +60,12 @@ export default function Card({
             |
           </span>
 
-          <button
-            className={styles.btnAddToCart}
-            aria-label={`Add ${albumTitle} by ${albumArtist} to cart`}
-            onClick={onAddToCart}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                d="M1.5 3H3.25L4 6M4 6L6.5 16H19.5L22 6H4Z"
-                stroke="#1A1A1A"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6.5 21C7.32843 21 8 20.3284 8 19.5C8 18.6716 7.32843 18 6.5 18C5.67157 18 5 18.6716 5 19.5C5 20.3284 5.67157 21 6.5 21Z"
-                stroke="#1A1A1A"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M19.5 21C20.3284 21 21 20.3284 21 19.5C21 18.6716 20.3284 18 19.5 18C18.6716 18 18 18.6716 18 19.5C18 20.3284 18.6716 21 19.5 21Z"
-                stroke="#1A1A1A"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M11 11H15M13 13V9"
-                stroke="#1A1A1A"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+          <AddToCartButton
+            variant={ADD_BTN_VARIANT.ICON}
+            albumTitle={albumTitle}
+            albumArtist={albumArtist}
+            onClick={handleClick}
+          />
         </div>
       </div>
     </article>
